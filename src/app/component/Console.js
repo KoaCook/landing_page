ff754807eca5bc26300f01cfb640d9128179c951
hpcao299 <caohoangphuc2909@gmail.com>
2025-02-26 19:41:27 +0700

    add console client component

---
+++ src/app/component/Console.js
@@ -0,0 +1,12 @@
+'use client';
+
+const Console = () => {
+    console.log(
+        '%cHi developers, if you want to contribute to this project visit %chttps://github.com/KoaCook',
+        'color: #3b82f6; font-size: 16px; font-weight: bold;',
+        'color: #16a34a; font-size: 16px; text-decoration: underline;'
+    );
+    return <></>;
+};
+
+export default Console;
