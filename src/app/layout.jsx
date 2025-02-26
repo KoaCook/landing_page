40114c0ef3ab19291792919897f761a5a50685d8
James Cao <caohoangphuc2909@gmail.com>
2025-02-26 19:31:59 +0700

    add console.log for developers to see

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,6 +23,12 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    console.log(
+  "%cHi developers, if you want to contribute to this project visit %chttps://github.com/KoaCook",
+  "color: #3b82f6; font-size: 16px; font-weight: bold;",
+  "color: #16a34a; font-size: 16px; text-decoration: underline;"
+);
+    
     return (
         <html lang="en" dir="LTR" className="scroll-smooth">
             <body className={`${rubic.variable}`}>
